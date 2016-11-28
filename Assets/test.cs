64ea6ee7578e9131017b8f14967a382007a86509
satooon <lucklaw9210@gmail.com>
2016-11-28 17:33:01 +0900

    Initial commit

---
+++ Assets/test.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Reflection;
+
+public class test : MonoBehaviour {
+
+    private hoge _hoge;
+
+    void Awake()
+    {
+        var targetMethod = typeof(hoge).GetMethod ("getTxt", BindingFlags.Instance | BindingFlags.Public);
+        var replacedMethod = typeof(fuga).GetMethod ("getTxt", BindingFlags.Instance | BindingFlags.Public);
+
+        try {
+            MethodUtil.ExchangeFunctionPointer (targetMethod, replacedMethod);
+        } catch (Exception e) {
+            Debug.LogError (e.ToString());
+        }
+
+        this._hoge = new hoge ();
+    }
+	
+    void OnGUI()
+    {
+        GUILayout.Label (this._hoge.getTxt());
+    }
+}
+
+public class hoge {
+    public string getTxt() {
+        return "hoge text";
+    }
+}
+
+public class fuga {
+    public string getTxt() {
+        return "fuga text";
+    }
+}
+
+public static class MethodUtil
+{
+    public static void ExchangeFunctionPointer(MethodInfo method0, MethodInfo method1)
+    {
+        unsafe
+        {
+            var functionPointer0 = method0.MethodHandle.Value.ToPointer();
+            var functionPointer1 = method1.MethodHandle.Value.ToPointer();
+            var tmpPointer = *((int*)new IntPtr(((int*)functionPointer0 + 1)).ToPointer());
+            *((int*)new IntPtr(((int*)functionPointer0 + 1)).ToPointer()) = *((int*)new IntPtr(((int*)functionPointer1 + 1)).ToPointer());
+            *((int*)new IntPtr(((int*)functionPointer1 + 1)).ToPointer()) = tmpPointer;
+        }
+    }
+}
+
